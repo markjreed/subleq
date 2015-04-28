bcf94f771492114995b698cc9a6d925eb03fc9da
Mark J. Reed <markjreed@gmail.com>
2015-04-28 09:11:23 -0400

    add Python

---
+++ subleq.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+memory = map(int, open(sys.argv[1]).read().replace("\n"," ")[:-1].split(' '))
+ip = 0
+while ip >= 0 and ip < len(memory):
+  a, b, c = memory[ip:ip+3]
+  ip += 3
+  if a < 0:
+    memory[b] = ord(sys.stdin.read(1))
+  elif b < 0:
+    sys.stdout.write(chr(memory[a]))
+  else:
+    memory[b] -= memory[a]
+    if memory[b] <= 0:
+      ip = c;
